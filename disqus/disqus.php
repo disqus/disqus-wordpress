276c4b44765f25c4f0bcc9469b6eece7bc7554b4
Artem Russakovskii <admin@beerpla.net>
2012-04-30 17:05:58 -0700

    Disable internal Wordpress commenting if Disqus is enabled - this prevents spam bots from commenting using POST requests to /wp-comments-post.php.

--- disqus/disqus.php
+++ disqus/disqus.php
@@ -1488,4 +1488,20 @@ function dsq_uninstall_database($version=0) {
         $wpdb->query("DROP INDEX disqus_dupecheck ON `".$wpdb->prefix."commentmeta`;");
     }
 }
+
+/**
+* Disable internal Wordpress commenting if Disqus is enabled - this prevents spam bots from
+* commenting using POST requests to /wp-comments-post.php.
+*
+* @param int $comment_post_ID
+* @return int
+*/
+function my_pre_comment_on_post($comment_post_ID) {
+    if (dsq_can_replace()) {
+        wp_die( __('Sorry, the built-in commenting system is disabled because Disqus is active.') );
+    }
+    return $comment_post_ID;
+}
+add_action('pre_comment_on_post', 'dsq_pre_comment_on_post');
+
 ?>
