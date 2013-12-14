aeefcd2d5d26860694b003db919fef366f7582d4
Cott Lang <cott@internetstaff.com>
2013-12-14 11:25:19 -0700

    Change default timeout of 5 seconds to 60 seconds so that imports from the plugin actually work.

--- disqus/lib/wp-api.php
+++ disqus/lib/wp-api.php
@@ -79,6 +79,7 @@ class DisqusWordPressAPI {
             DISQUS_IMPORTER_URL . 'api/import-wordpress-comments/',
             array(
                 'method' => 'POST',
+                'timeout' => 60,
                 'body' => array(
                     'forum_url' => $this->short_name,
                     'forum_api_key' => $this->forum_api_key,
